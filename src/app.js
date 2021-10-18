4d3f4444daad7f1c225a1203cca863fae904fdc1
ceylan öztürk <ceylann1905@outlook.com>
2021-10-19 00:11:11 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,18 @@
+console.log("Merhaba kodlama.io")
+let dolarDun=9.20
+
+let dolarBugun=9.30
+
+{
+    let dolarDun=9.10
+}
+
+let konutKredileri=["Konut kredisi","Emlak Konut kredisi","Kamu Konut Kredisi","Özel1 Konut Kredisi"]
+console.log(konutKredileri)
+
+console.log("<ul>")
+for(let i=0;i<konutKredileri.length;i++)
+{
+console.log("<li>"+konutKredileri[i]+"</li>")
+}
+console.log("</ul>")
